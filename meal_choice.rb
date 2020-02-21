70c8fbf273c51d0997a7ba53ac9ddf52297c776a
Laura Messa Travin <laura.travin@gmail.com>
2020-02-21 15:39:33 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,6 +1,6 @@
 
 def meal_choice(var1,var2, protein="meat")
   puts "What a nutritious meal!"
-  "A plate of #{protein} with #{var1} and #{var2}."
+  return "A plate of #{protein} with #{var1} and #{var2}."
 
 end
